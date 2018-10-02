f8cd87fcfe193831dcb985c8eab4aa0fd4624589
dzonib <nikolabosnjak381@gmail.com>
2018-10-02 14:15:11 +0200

    event date finished MOMENT powa

--- src/featured/Countdown.js
+++ src/featured/Countdown.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Slide from 'react-reveal/Slide';
 import CountdownHelper from './ContdownHelper';
+import moment from 'moment'; 
 
 class Countdown extends Component {
 	state = {
@@ -8,11 +9,39 @@ class Countdown extends Component {
     days: '0',
     hours: '0',
     minutes: '0',
-    secundes: '0'
+    seconds: '0'
   };
 
   getTimeUntill = (deadline) => {
-    const time = Date.parse(deadline) - Date.parse(new Date());
+
+    // http://zetcode.com/javascript/momentjs/
+    // moment tut
+
+    const time = moment(deadline);
+    const date = moment([]);
+
+    // const timeTest = moment(Date.parse(new Date())).format("dddd, MMMM, D, YYYY, s");
+
+    const difference = time.diff(date);
+
+    const momentizeDifference = moment(difference);
+
+    const dayDiff = time.diff(date, 'days');
+
+    console.log(dayDiff);
+    
+
+    const days = dayDiff;
+    const hours = momentizeDifference.get('hour');
+    const minutes = momentizeDifference.get('minute');
+    const seconds = momentizeDifference.get('second');
+
+    this.setState({
+      days,
+      hours, 
+      minutes,
+      seconds,
+    }); 
 
   }
 
@@ -21,16 +50,17 @@ class Countdown extends Component {
   }
 
 	render() {
+    const {days, hours, minutes, seconds} = this.state;
 		return (
 			<Slide left>
 				<div className="countdown_wrapper">
 					<div className="countdown_top">Event starts in</div>
 					<div className="countdown_bottom">
 
-            <CountdownHelper number={23} letter="days" />
-            <CountdownHelper number={2} letter="hours" />
-            <CountdownHelper number={30} letter="minutes" />
-            <CountdownHelper number={54} letter="secundes" />
+            <CountdownHelper number={days} letter="days" />
+            <CountdownHelper number={hours} letter="hours" />
+            <CountdownHelper number={minutes} letter="minutes" />
+            <CountdownHelper number={seconds} letter="seconds" />
 					
 		
 					</div>
